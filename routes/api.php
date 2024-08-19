fbae40a90ca542d01db62ccd08e147014d6c2061
Adipramana <herryprasetyo.dev@gmail.com>
2024-08-19 20:44:00 +0700

    backendLaravel webhozz

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiCategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('hello', function () {
+    return  'Hello, World!';
+});
+
+//api categories 
+Route::get('categories', [ApiCategoryController::class,'index']);
+Route::post('categories', [ApiCategoryController::class,'store']);
+Route::delete('destroycategories/{id}', [ApiCategoryController::class,'destroy']);
